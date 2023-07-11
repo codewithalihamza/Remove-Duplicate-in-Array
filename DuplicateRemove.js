1240a9fb7f2be129976bce7c74243674d267a500
Ali Hamza <59261458+codewithalihamza@users.noreply.github.com>
2023-07-11 10:57:31 +0500

    Add files via upload

---
+++ DuplicateRemove.js
@@ -0,0 +1,15 @@
+let arr=[1,3,2,3,1,5,5]
+let newArr=[]
+for(let i=0;i<arr.length;i++){
+    let duplicate=false
+    for(let j=i+1;j<arr.length;j++){
+        if(arr[i]==arr[j]){
+            duplicate=true
+            break
+        }
+    }
+    if(!duplicate){
+        newArr.push(arr[i])
+    }
+}
+console.log("Removing all Duplicate",newArr)
